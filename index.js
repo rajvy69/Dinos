2a5bc03740970f320ffac25e321067d6c50187d1
Rohit Vyas <navylover2916@gmail.com>
2021-07-11 18:39:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -60,10 +60,10 @@ client.on('ready', () => {
 
 //Message Event
 client.on('message', async message => {
-  const xp = require('./events&functions/xp')
+  
   if (!message.guild) return;
   const prefix = process.env.prefix;
-  xp(message)
+  
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
